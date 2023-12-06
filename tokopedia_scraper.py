7332d39aa1f1a440893e605260cebb98da82143f
rzkadltm <you@example.com>
2023-12-06 23:14:10 +0700

    Tokopedia Scraper Version 1.0.1

--- tokopedia_scraper.py
+++ tokopedia_scraper.py
@@ -21,7 +21,9 @@ class TokopediaScraper:
         self.page_url = '&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&navsource='
         self.scroll_increment = 200
         self.scroll_duration = 0.3
-        self.page_now = 0
+        self.page_now = 1
+        self.attempts = 3
+        self.attempt_count = 0
     
     # ------------------------------------------------------------
     #                        Main Function
@@ -30,97 +32,105 @@ class TokopediaScraper:
         url = f'{self.base_url}{keywords}{self.page_url}'
         self.driver.maximize_window()
         self.driver.get(url)
-        try:
-            for _ in range(pages):
-                self.page_now += 1
-                total_height = int(self.driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );"))
-                time.sleep(1)
-                for i in range(0, total_height, self.scroll_increment):
-                    self.driver.execute_script(f"window.scrollBy(0, {self.scroll_increment});")
-                    time.sleep(self.scroll_duration)
+        while self.attempt_count < self.attempts and self.page_now <= pages:
+            try:
+                for _ in range(pages):
+                    total_height = int(self.driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );"))
+                    time.sleep(1)
+                    for i in range(0, total_height, self.scroll_increment):
+                        self.driver.execute_script(f"window.scrollBy(0, {self.scroll_increment});")
+                        time.sleep(self.scroll_duration)
 
-                soup = BeautifulSoup(self.driver.page_source, 'html.parser')
-                elements = soup.find_all('div', class_='css-llwpbs')
+                    soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+                    elements = soup.find_all('div', class_='css-llwpbs')
 
-                for element in elements:
-                    # -------------------- Name --------------------
-                    try:
-                        nama_product = element.find('div', class_='prd_link-product-name')
-                        if nama_product:
-                            nama_product = nama_product.text.strip()
-                    except:
-                        nama_product = None
-                    # -------------------- Image --------------------
-                    try:
-                        img = element.find('img', class_='css-1q90pod')
-                        if img:
-                            img = img['src']
-                    except:
-                        img = None
-                    # -------------------- Terjual --------------------
-                    try:
-                        terjual = element.find('span', class_='prd_label-integrity')
-                        if terjual:
-                            terjual = terjual.text.strip()
-                    except:
-                        terjual = None
-                    # -------------------- Rating --------------------
-                    try:
-                        rating = element.find('span', class_='prd_rating-average-text')
-                        if rating:
-                            rating = rating.text.strip()
-                    except:
-                        rating = None
-                    # -------------------- Link Product --------------------
-                    try:
-                        link_product = element.find('a', class_='pcv3__info-content')
-                        if link_product:
-                            link_product = link_product.get('href')
-                    except:
-                        link_product = None
-                    # -------------------- Lokasi --------------------
-                    try:
-                        lokasi = element.find('span', class_='prd_link-shop-loc')
-                        if lokasi:
-                            lokasi = lokasi.text.strip()
-                    except:
-                        lokasi = None
-                    # -------------------- Nama Toko --------------------
-                    try:
-                        nama_toko = element.find('span', class_='prd_link-shop-name')
-                        if nama_toko:
-                            nama_toko = nama_toko.text.strip()
-                    except:
-                        nama_toko = None
-                        
-                    self.datas.append({
-                        'id': self.id,
-                        'nama_product': nama_product,
-                        'img': img,
-                        'terjual': terjual,
-                        'rating': rating,
-                        'link_product': link_product,
-                        'lokasi': lokasi,
-                        'nama_toko': nama_toko
-                    })
-                    self.id += 1
-            
-                for data in self.datas:
-                    print(f'---------- Product-{data['id']} ----------')
-                    pprint(data)
-                    print('\n')
+                    for element in elements:
+                        # -------------------- Name --------------------
+                        try:
+                            nama_product = element.find('div', class_='prd_link-product-name')
+                            if nama_product:
+                                nama_product = nama_product.text.strip()
+                        except:
+                            nama_product = None
+                        # -------------------- Image --------------------
+                        try:
+                            img = element.find('img', class_='css-1q90pod')
+                            if img:
+                                img = img['src']
+                        except:
+                            img = None
+                        # -------------------- Terjual --------------------
+                        try:
+                            terjual = element.find('span', class_='prd_label-integrity')
+                            if terjual:
+                                terjual = terjual.text.strip()
+                        except:
+                            terjual = None
+                        # -------------------- Rating --------------------
+                        try:
+                            rating = element.find('span', class_='prd_rating-average-text')
+                            if rating:
+                                rating = rating.text.strip()
+                        except:
+                            rating = None
+                        # -------------------- Link Product --------------------
+                        try:
+                            link_product = element.find('a', class_='pcv3__info-content')
+                            if link_product:
+                                link_product = link_product.get('href')
+                        except:
+                            link_product = None
+                        # -------------------- Lokasi --------------------
+                        try:
+                            lokasi = element.find('span', class_='prd_link-shop-loc')
+                            if lokasi:
+                                lokasi = lokasi.text.strip()
+                        except:
+                            lokasi = None
+                        # -------------------- Nama Toko --------------------
+                        try:
+                            nama_toko = element.find('span', class_='prd_link-shop-name')
+                            if nama_toko:
+                                nama_toko = nama_toko.text.strip()
+                        except:
+                            nama_toko = None
+                            
+                        self.datas.append({
+                            'id': self.id,
+                            'nama_product': nama_product,
+                            'img': img,
+                            'terjual': terjual,
+                            'rating': rating,
+                            'link_product': link_product,
+                            'lokasi': lokasi,
+                            'nama_toko': nama_toko
+                        })
+                        self.id += 1
                 
-                wait = WebDriverWait(self.driver, 5)
-                button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/div/div[2]/div[5]/nav/ul/li[11]/button')))
-                time.sleep(0.3)
-                button.location_once_scrolled_into_view
-                time.sleep(0.3)
-                self.driver.execute_script(f"window.scrollBy(0, -250);")
-                button.click()
+                    for data in self.datas:
+                        print(f'---------- Product-{data['id']} ----------')
+                        pprint(data)
+                        print('\n')
+                    
+                    wait = WebDriverWait(self.driver, 5)
+                    button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/div/div[2]/div[5]/nav/ul/li[11]/button')))
+                    time.sleep(0.3)
+                    button.location_once_scrolled_into_view
+                    time.sleep(0.3)
+                    self.driver.execute_script(f"window.scrollBy(0, -250);")
+                    button.click()
+                    time.sleep(3)
+                    self.attempt_count = 0
+                    self.page_now += 1
+            except Exception as e:
+                print(f'Error executing on page: {self.page_now}. Error: {e}')
+                self.attempt_count += 1
+                self.driver.refresh()
                 time.sleep(3)
-        except:
-            print(f'Error executing on page: {self.page_now}')
-        
+                self.driver.execute_script("window.scrollTo(0, 0);")
+        else:
+            print(f"You have scraped {pages} pages.")
+
         # ------------------------------------------------------------
         #                 Replace | to , in datas
         # ------------------------------------------------------------
